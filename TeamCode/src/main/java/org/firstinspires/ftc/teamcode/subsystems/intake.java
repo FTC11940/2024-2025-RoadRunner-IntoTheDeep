de75429617647500f740b5bccf1e1f87aa253fab
ima_dinosaur <134715805+Isaak3@users.noreply.github.com>
2024-10-17 16:40:30 -0400

    Setting up the structure for intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
@@ -1,4 +1,44 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class intake {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+public class intake extends LinearOpMode {
+    //Equation for finding 160 degrees: 0.88889*X and X = what position the servo is in when it is 180 degrees.
+    //servoName.setPosition(0.88889);
+    Servo intakeServo;
+    DcMotor intakeMotor;
+    TouchSensor intakeTouch;
+    //position to pick up pieces (for picking up pieces) 0 degrees
+    public static final double Picking_Up = 0;
+    //Dropping pieces into the bucket (for scoring in the baskets) about 160 degrees
+    public static final double Dropping_Into_Bucket = 0.888889;
+    //The intake (For picking up pieces using the wheel)
+    public static final double Intake = 1;
+    //The release (for dropping pieces into the observation zone (specimen zone))
+    public static final double Release = -1;
+
+
+    //@Override
+    public void runOpMode() {
+
+        intakeServo = hardwareMap.servo.get("intakeServo");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        intakeTouch = hardwareMap.get(TouchSensor.class, "intakeTouch");
+
+
+        //Set the starting position
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+
+
+    }
 }
