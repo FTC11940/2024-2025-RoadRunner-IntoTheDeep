fdde341693f70d3017ee03de6139f80ce533a96d
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-10-18 07:49:54 -0400

    Making intake a subsystem class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
@@ -1,24 +1,30 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import org.firstinspires.ftc.teamcode.subsystems.intake; // Added by CoPilot
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-public class intake extends LinearOpMode {
+// Removed the extended LinearOpMode
+public class intake {
     //Equation for finding 160 degrees: 0.88889*X and X = what position the servo is in when it is 180 degrees.
     //servoName.setPosition(0.88889);
+    
     Servo intakeServo;
     DcMotor intakeMotor;
     TouchSensor intakeTouch;
+    
     //position to pick up pieces (for picking up pieces) 0 degrees
     public static final double Picking_Up = 0;
+    
     //Dropping pieces into the bucket (for scoring in the baskets) about 160 degrees
     public static final double Dropping_Into_Bucket = 0.888889;
+    
     //The intake (For picking up pieces using the wheel)
     public static final double Intake = 1;
+
     //The release (for dropping pieces into the observation zone (specimen zone))
     public static final double Release = -1;
 
