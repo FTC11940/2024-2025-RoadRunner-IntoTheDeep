04abd722918ca0a9528440685f77a9a237ac5ee0
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-10-20 08:35:35 -0400

    Update IntakeSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -6,20 +6,20 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 // Removed the extended LinearOpMode
-public class intake {
+public class IntakeSubsystem {
     //Equation for finding 160 degrees: 0.88889*X and X = what position the servo is in when it is 180 degrees.
     //servoName.setPosition(0.88889);
-    
+
     Servo intakeServo;
     DcMotor intakeMotor;
     TouchSensor intakeTouch;
-    
+
     //position to pick up pieces (for picking up pieces) 0 degrees
     public static final double Picking_Up = 0;
-    
+
     //Dropping pieces into the bucket (for scoring in the baskets) about 160 degrees
     public static final double Dropping_Into_Bucket = 0.888889;
-    
+
     //The intake (For picking up pieces using the wheel)
     public static final double Intake = 1;
 
@@ -34,14 +34,7 @@ public class intake {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         intakeTouch = hardwareMap.get(TouchSensor.class, "intakeTouch");
 
-
         //Set the starting position
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-
-        }
 
 
     }
