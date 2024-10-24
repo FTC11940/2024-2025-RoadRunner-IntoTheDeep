28444fcef1a76476b8b87dab0feb829beb9d06e3
Isaak <134715805+Isaak3@users.noreply.github.com>
2024-10-24 17:00:19 -0400

    Update SlidesSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
@@ -1,5 +1,35 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+import com. quilcomm.robotcore.hardware.dcmotor;
+import com. quilcomm.robotcore.hardware.dcmotor;
+import com. quilcomm.robotcore.hardware.hardwaremap
+
 
 public class SlidesSubsystem {
-    
-}
+
+
+    private final dcmotor somemotor;
+
+    public SlidesSubsystem(hardwaremap) {
+        somemotor = hardwaremap.get(dcmotor.class, "leftback");
+        somemotor.setdirection(dcmotor.direction.forward);
+        somemotor.setpower(0);
+
+    }
+
+
+
+    public void rotatemotorreverse(double power) {
+        somemotor.setpower(-power);
+    }
+
+
+    public void stopmotor() {
+        somemotor(0);
+    }
+
+
+    public void incrementmotor(double rotsomedegrees) {
+
+    }
+
+} //end of class
