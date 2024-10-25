a082f6c7b346ccacd86bcef8e14960c99c0ab39c
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-10-24 21:23:17 -0400

    Added FIXME and TODO, see also Error messages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+
+// FIXME check spelling of imports
 import com. quilcomm.robotcore.hardware.dcmotor;
 import com. quilcomm.robotcore.hardware.dcmotor;
+// FIXME check spelling of imports, and punctuation
 import com. quilcomm.robotcore.hardware.hardwaremap
 
 
@@ -9,6 +12,7 @@ public class SlidesSubsystem {
 
     private final dcmotor somemotor;
 
+    // FIXME check reference
     public SlidesSubsystem(hardwaremap) {
         somemotor = hardwaremap.get(dcmotor.class, "leftback");
         somemotor.setdirection(dcmotor.direction.forward);
@@ -16,8 +20,7 @@ public class SlidesSubsystem {
 
     }
 
-
-
+    // TODO Create a method to move the slides to different positions
     public void rotatemotorreverse(double power) {
         somemotor.setpower(-power);
     }
@@ -31,5 +34,6 @@ public class SlidesSubsystem {
     public void incrementmotor(double rotsomedegrees) {
 
     }
+
 
 } //end of class
