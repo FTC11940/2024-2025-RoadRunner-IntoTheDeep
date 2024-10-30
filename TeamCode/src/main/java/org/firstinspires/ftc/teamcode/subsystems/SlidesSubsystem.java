036d75441d62024aeb3e2dffc06f30bfdcfdba3c
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-10-29 20:38:32 -0400

    Update SlidesSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
@@ -1,9 +1,40 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 
 public class SlidesSubsystem {
 
-    public SlidesSubsystem(HardwareMap hardwareMap) {
+
+    private final DcMotor somemotor;
+
+    public SlidesSubsystem(HardwareMap hardwaremap) {
+        somemotor = hardwaremap.get(DcMotor.class, "leftback");
+        somemotor.setDirection(DcMotor.Direction.FORWARD);
+        somemotor.setPower(0);
+
     }
-}
+
+    // TODO Create a method to move the slides to different positions
+    public void rotatemotorreverse(double power) {
+        somemotor.setPower(-power);
+    }
+
+
+    public void stopmotor() {
+        somemotor(0);
+    }
+
+    private void somemotor(int i) {
+
+    }
+
+
+    public void incrementmotor(double rotsomedegrees) {
+
+    }
+
+
+} //end of class
