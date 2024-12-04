3260770ab10345698acecd37a3afc6732013b709
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-12-03 23:00:35 -0500

    Revert "ha"
    This reverts commit 42fc09ba2af0e6a2361dbe8f922feadd6370eace.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FollowerPIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FollowerPIDTuner.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -51,7 +50,7 @@ public class FollowerPIDTuner extends LinearOpMode {
                     .forward(DISTANCE)
                     .turn(Math.toRadians(90))
                     .build();
-            drive.followTrajectory(trajSeq);
+            drive.followTrajectorySequence(trajSeq);
         }
     }
 }
